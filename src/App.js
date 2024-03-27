f2a94497e89a9149c4bf74baabf58ad79a0791d8
AbdMajidKHn <majidkhan@gmail.com>
2024-03-27 16:53:15 +0500

    yoo

--- src/App.js
+++ src/App.js
@@ -1,51 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-// import logo from '/Logos/Logo.png'; //
+// import './App.css';
+
 function App() {
   const [showVideo, setShowVideo] = useState(true);
   const [videoWidth, setVideoWidth] = useState('50%');
   const [textOpacity, setTextOpacity] = useState(1);
+  const [hoveredCard, setHoveredCard] = useState(null);
+  // Define a state to control the video movement
+  const [videoStyle, setVideoStyle] = useState({});
 
-
-  // This effect hides the video and shows the website content after 5 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowVideo(false);
-    }, 2000); // Adjust this duration as needed
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
       const windowHeight = window.innerHeight;
 
-      // Calculate the new width based on the scroll position
-      const newWidth = `${Math.max(50, 50 + (scrollTop / windowHeight) * 50)}%`;
+      const newWidth = `${Math.min(100, 50 + (scrollTop / windowHeight) * 50)}%`;
 
-      // Update the state to set the new width
       setVideoWidth(newWidth);
-      // Update text opacity based on scroll position
       const newOpacity = Math.max(0, 1 - (scrollTop / windowHeight));
       setTextOpacity(newOpacity);
     };
 
     window.addEventListener('scroll', handleScroll);
 
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  // This effect adjusts the video width on scroll
 
   const cardNames = [
-    'RENEWABLE ENERGY', 'SMART SYSTEMS', 'INNOVATIVE DESIGN', 'RECYCING', 'ECO-FRIENDLY MATERIALS', 'EFFICIENT EQUIPMENT', 'INTEGRATED SOLUTIONS', 'PREDICTIVE MAINTENANCE', 'BLOGS', 'COMPANY PROFILE', 'MAJOR ADVANTAGES', 'ABOUT US', 'VALUABLE PARTNERSHIPS', 'A COMPLETE ECO CYCLE', 'ROAD MAP TO SUCCESS', 'CONTACT US',// ... add more names
+    'RENEWABLE ENERGY', 'SMART SYSTEMS', 'INNOVATIVE DESIGN', 'RECYCLING', 'ECO-FRIENDLY MATERIALS', 'EFFICIENT EQUIPMENT', 'INTEGRATED SOLUTIONS', 'PREDICTIVE MAINTENANCE', 'BLOGS', 'COMPANY PROFILE', 'MAJOR ADVANTAGES', 'ABOUT US', 'VALUABLE PARTNERSHIPS', 'A COMPLETE ECO CYCLE', 'ROAD MAP TO SUCCESS', 'CONTACT US',
   ];
+  // Function to handle mouse enter and leave events
+  const handleMouseEnter = () => {
+    setVideoStyle({
+      transform: 'translateY(-10%)', // Moves the video up by 10%
+      transition: 'transform 0.5s ease-in-out' // Smooth transition for the movement
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setVideoStyle({
+      transform: 'translateY(0)', // Returns the video to its original position
+      transition: 'transform 0.5s ease-in-out' // Smooth transition for the movement
+    });
+  };
   return (
     <div style={{ width: '100%', height: '100%' }}>
       {showVideo ? (
-        // Fullscreen video with brand name 'EGB'
         <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
           <video
             className="video-frame"
@@ -66,15 +76,13 @@ function App() {
               top: '50%',
               left: '50%',
               transform: 'translate(-50%, -50%)',
-              maxHeight: '50%', // Adjust as needed
-              maxWidth: '50%', // Adjust as needed
+              maxHeight: '50%',
+              maxWidth: '50%',
             }}
           />
         </div>
       ) : (
-        // Website content after the video
         <div className="app-container" style={{ width: '100%', height: '100vh', display: 'flex' }}>
-          {/* Left side with video */}
           <div className="video-container" style={{ width: videoWidth, height: '900px', position: 'relative', opacity: '1.5' }}>
             <video
               id="videoFrame"
@@ -97,15 +105,11 @@ function App() {
                 top: '50%',
                 left: '50%',
                 transform: 'translate(-50%, -50%)',
-                maxHeight: '50%', // Adjust as needed
-                maxWidth: '50%', // Adjust as needed
+                maxHeight: '50%',
+                maxWidth: '50%',
               }}
             />
           </div>
-
-
-
-          {/* Right side with text */}
           <div
             className="text-container"
             style={{
@@ -114,9 +118,9 @@ function App() {
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
-              paddingTop: '20%',
+              paddingTop: '3%',
               transition: 'opacity 0.5s ease-in-out',
-              opacity: textOpacity, // Use the state variable here
+              opacity: textOpacity,
             }}
           >
             <h1
@@ -130,14 +134,12 @@ function App() {
                 margin: '0',
               }}
             >
-              Ever Green Is on a mission to use the world's resources more efficietly, and to create more energy-efficient and healthier homes and buildings.
+              Ever Green Is on a mission to use the world's resources more efficiently, and to create more energy-efficient and healthier homes and buildings.
             </h1>
           </div>
-
         </div>
-
       )}
-      <div className="video-container" style={{ width: '100%', height: '67vh', position: 'relative' }}>
+      <div className="video-container" style={{ width: '100%', height: '203vh', position: 'relative', borderRadius: '20px' }}>
         <video
           id="videoFrame"
           className="video-frame"
@@ -145,26 +147,92 @@ function App() {
           autoPlay
           loop
           muted
-          style={{ width: '100%', height: '300%', objectFit: 'cover', marginTop: '100px' }}
+          style={{ width: '99.5%', height: '99%', objectFit: 'cover', marginTop: '10px', borderRadius: '20px' }}
         >
           <source src="/Videos/Moving_Gradient_Background.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        {/* Grid container for cards */}
-
-        <div style={{ position: 'absolute', top: '0', left: '0', width: '99%', height: '99%', marginTop: '100px', display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'repeat(9, 1fr)', gap: '15px', padding: '10px' }}>
-  {/* Cards */}
-  {Array.from({ length: 16 }).map((_, index) => (
-    <div key={index} className="card" style={{ background: 'transparent', padding: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid #ccc', height: '350px' /* Adjust this value as needed */ }}>
-      {/* Content for each card */}
-      <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '24px', color: 'white' }}>{cardNames[index]}</p>
-    </div>
-  ))}
+        <div style={{ position: 'absolute', top: '0', left: '0', width: '99%', height: '99%', marginTop: '10px', display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'repeat(9, 1fr)', gap: '15px', padding: '10px' }}>
+          {Array.from({ length: 16 }).map((_, index) => (
+            <div
+              key={index}
+              className="card"
+              style={{
+                background: 'transparent',
+                padding: '20px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                border: '0.5px solid Grey',
+                height: '350px',
+                position: 'relative',
+                cursor: 'pointer',
+                overflow: 'hidden',
+                borderRadius: '20px'
+              }}
+              onMouseEnter={() => setHoveredCard(index)}
+              onMouseLeave={() => setHoveredCard(null)}
+            >
+              {hoveredCard === index && (
+                <div
+                  className="card-overlay"
+                  style={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    transition: 'background-color 0.3s ease-in-out',
+                  }}
+                >
+                  <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '24px', color: 'grey' }}>
+                    {cardNames[index]}
+                  </p>
+                  <p style={{ fontFamily: 'Helvetica', fontWeight: 'normal', fontSize: '16px', color: 'grey', textAlign: 'center' }}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac massa a arcu mollis ullamcorper.
+                  </p>
+                </div>
+              )}
+              <p style={{ fontFamily: 'Arial, sans-serif', fontWeight: 'bold', fontSize: '24px', color: 'white' }}>
+                {cardNames[index]}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div style={{ backgroundColor: 'white', padding: '20px', display: 'flex', height: '99%' , display: 'flex' }}>
+      <p
+              className="h1-title"
+              style={{
+                fontWeight: '50',
+                width:'30%',
+                fontSize: '20px',
+                fontStyle: 'normal',
+                fontFamily: 'Times New Roman, Times, serif',
+                padding: '20px',
+                margin: '0',
+              }}
+            >
+              Ever Green Is on a mission to use the world's resources more efficiently, and to create more energy-efficient and healthier homes and buildings.
+            </p>
+    <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'flex-end' }}>
+   
+   <img src="/Images/Where_Green_Meets_Growth.png" alt="Where Green Meets Growth" style={{ width: 'auto', maxHeight: '100px', alignSelf: 'center' , marginTop: '45px', marginLeft: '500px'}} />
+ </div>
+  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+    <img src="/Images/EcoInnovate.png" alt="Eco-Innovate" style={{ width: 'auto', maxHeight: '100px', margin: '10px' }} />
+    <img src="/Images/Redefining_a_Sustainable_Future.png" alt="Redefining a Sustainable Future" style={{ width: 'auto', maxHeight: '100px', margin: '10px' }} />
+  </div>
+  
 </div>
 
 
 
-      </div>
 
 
 
